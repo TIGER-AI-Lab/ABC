4b55c8629724dd8f21be262128c1794485ce6462
Ben Schneider <schnei19@myumanitoba.ca>
2025-02-28 19:47:55 -0500

    update code example

--- demo.py
+++
@@ -1,8 +0,0 @@
-import torch
-base_model = "Qwen/Qwen2-VL-7B-Instruct"
-pretrain_adapter = "TIGER-Lab/ABC-Qwen2VL-Pretrain"
-instuct_adapter = "TIGER-Lab/ABC-Qwen2VL-Instruct"
-
-
-from functional.embed_function import get_embed_function
-embed_function, model = get_embed_function(base_model, pretrain_adapter, instuct_adapter)
--- script/qwen_finetune.sh
+++
@@ -1,4 +0,0 @@
-GPU=$1
-PORT=$2
-CONFIG=$3
-deepspeed --include localhost:$GPU --master_port $PORT $VLLM/qwen/qwen_train.py $VLLM$CONFIG
